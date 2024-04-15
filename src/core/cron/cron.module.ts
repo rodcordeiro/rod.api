1b92dd7dbbe080bb5e2697971617015977c48843
Rodrigo Cordeiro <rodrigomendoncca@gmail.com>
2024-04-15 10:14:39 -0300

    chore: Create scheduled tasks. Pending work api request

--- src/core/cron/cron.module.ts
+++ src/core/cron/cron.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
 import { ScheduleModule } from '@nestjs/schedule';
+import { HttpModule } from '@nestjs/axios';
 
 import { JiraServices } from './services/jira.services';
 
 @Module({
-  imports: [ScheduleModule.forRoot()],
+  imports: [
+    HttpModule.register({
+      timeout: 5000,
+      maxRedirects: 5,
+    }),
+    ScheduleModule.forRoot(),
+  ],
   providers: [JiraServices],
 })
 export class CronModule {}
